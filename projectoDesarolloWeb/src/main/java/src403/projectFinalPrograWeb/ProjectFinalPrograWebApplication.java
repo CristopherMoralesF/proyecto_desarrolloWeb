60b26afa4e61127aec0761db212495f6cdf166c6
CristopherMoralesF <71911123+CristopherMoralesF@users.noreply.github.com>
2021-11-29 00:22:22 -0600

    Cris changes 11/29/2021
    Creation of main classes and index html, pending to validate how to include the HTML and replace all the old paths to the images and other objects.

--- projectoDesarolloWeb/src/main/java/src403/projectFinalPrograWeb/ProjectFinalPrograWebApplication.java
+++ projectoDesarolloWeb/src/main/java/src403/projectFinalPrograWeb/ProjectFinalPrograWebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectFinalPrograWebApplication {
 
 	public static void main(String[] args) {
+                //Se inicia el modelo para ejecutar el index de la pag
 		SpringApplication.run(ProjectFinalPrograWebApplication.class, args);
 	}
 
---
+++ projectoDesarolloWeb/src/main/java/src403/projectFinalPrograWeb/controller/Controller.java
@@ -0,0 +1,21 @@
+package src403.projectFinalPrograWeb.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+@Slf4j
+public class Controller {
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+
+        var inicio = "Hello"; 
+        model.addAttribute("inicio", inicio);
+
+        return "index";
+
+    }
+
+}
