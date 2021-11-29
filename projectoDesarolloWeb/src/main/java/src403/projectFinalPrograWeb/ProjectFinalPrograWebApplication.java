e2cae01022e95020e9c6e706c4e72fb78f449af1
CristopherMoralesF <71911123+CristopherMoralesF@users.noreply.github.com>
2021-11-29 00:08:46 -0600

    Project Creation
    The base for all the project.

---
+++ projectoDesarolloWeb/src/main/java/src403/projectFinalPrograWeb/ProjectFinalPrograWebApplication.java
@@ -0,0 +1,13 @@
+package src403.projectFinalPrograWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFinalPrograWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFinalPrograWebApplication.class, args);
+	}
+
+}
---
+++ projectoDesarolloWeb/src/test/java/src403/projectFinalPrograWeb/ProjectFinalPrograWebApplicationTests.java
@@ -0,0 +1,13 @@
+package src403.projectFinalPrograWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFinalPrograWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
