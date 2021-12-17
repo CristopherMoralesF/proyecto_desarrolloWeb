1aa2db3130a61c8202d8f09791bb32cb3789a6d9
CristopherMoralesF <71911123+CristopherMoralesF@users.noreply.github.com>
2021-12-17 12:02:35 -0600

    Cris changes 17/12/2021
    Final changes.

--- projectoDesarolloWeb/src/main/java/src403/projectFinalPrograWeb/Tratamientos/ControllerTratamientos.java
+++ projectoDesarolloWeb/src/main/java/src403/projectFinalPrograWeb/Tratamientos/ControllerTratamientos.java
@@ -19,25 +19,32 @@ public class ControllerTratamientos {
     @GetMapping("/Tratamientos/{id_usuarios}/{id_usuario}")
     public String listaTratamientos(@PathVariable("id_usuarios") int id,@PathVariable("id_usuario") int id_usuario, Model model) {
 
-        var tratamiento = tratamientosService.getTratamientosUsuario(id);
+        var tratamientos = tratamientosService.getTratamientosUsuario(id);
 
         Usuario usuario = new Usuario();
         usuario.setId_usuarios(Long.parseLong(String.valueOf(id_usuario)));
-
+        Tratamientos tratamiento = new Tratamientos(); 
+        tratamiento.setId_Usuario(id);
+        
+        
+        model.addAttribute("tratamiento",tratamiento);
         model.addAttribute("usuario", usuario);
-        model.addAttribute("tratamiento", tratamiento);
+        model.addAttribute("tratamientos", tratamientos);
 
         return "Tratamientos";
 
     }
 
-    @GetMapping("/Crear Tratamientos/{id}")
-    public String ingresar(Tratamientos tratamientos,@PathVariable("id") long id, Model model) {
+    @GetMapping("/Crear Tratamientos/{id}/{id_log}")
+    public String ingresar(Tratamientos tratamientos,@PathVariable("id") int id,@PathVariable("id_log") int id_log, Model model) {
         
         Usuario usuario = new Usuario(); 
-        usuario.setId_usuarios(id);
+        usuario.setId_usuarios(Long.parseLong(String.valueOf(id_log)));
+        
+        tratamientos.setId_Usuario(id);
         
         model.addAttribute("usuario", usuario);
+        model.addAttribute("tratamientos",tratamientos);
         
         return "actualizar_tratamientos";
     }
@@ -47,22 +54,22 @@ public class ControllerTratamientos {
         return "actualizar_tratamientos";
     }
 
-    @GetMapping("/guardar tratamientos")
-    public String guardar(Tratamientos tratamientos) {
+    @GetMapping("/guardar tratamientos/{id}/{id_Usuario}")
+    public String guardar(Tratamientos tratamientos,@PathVariable("id") int id, @PathVariable("id_Usuario") int id_Usuario) {
         tratamientosService.guardar(tratamientos);
-        return "redirect:/";
+        return "redirect:/Tratamientos/" + String.valueOf(id) + "/" + String.valueOf(id_Usuario);
     }
 
-    @GetMapping("/editar/{id_tratamientos}")
+    @GetMapping("/editar/{id_tratamientos}/{id_usuario}")
     public String editar(Tratamientos tratamientos, Model model) {
         model.addAttribute("tratamientos", tratamientos);
         return "actualizar_tratamientos";
     }
 
-    @GetMapping("/eliminar/{id_tratamientos}")
-    public String eliminar(Tratamientos tratamientos) {
+    @GetMapping("/Eliminar Tratamiento/{id_tratamientos}/{id_usuario}/{id_usuariolog}")
+    public String eliminar(Tratamientos tratamientos,@PathVariable("id_usuario") int id_usuario,@PathVariable("id_usuariolog") int id_usuariolog) {
         tratamientosService.eliminar(tratamientos);
-        return "redirect:/";
+        return "redirect:/Tratamientos/" + id_usuario + "/" + String.valueOf(id_usuariolog);
     }
 
 }
