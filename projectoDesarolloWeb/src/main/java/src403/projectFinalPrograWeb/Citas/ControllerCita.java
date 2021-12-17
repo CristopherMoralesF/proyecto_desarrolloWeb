126ce5748df4bfc5967f783b9ef43485db6ba3ad
CristopherMoralesF <71911123+CristopherMoralesF@users.noreply.github.com>
2021-12-17 03:25:05 -0600

    Cris changes 17/12/2021
    All is working, just pending treatments.

--- projectoDesarolloWeb/src/main/java/src403/projectFinalPrograWeb/Citas/ControllerCita.java
+++ projectoDesarolloWeb/src/main/java/src403/projectFinalPrograWeb/Citas/ControllerCita.java
@@ -14,35 +14,54 @@ public class ControllerCita {
 
     @Autowired
     private CitaService citaService;
-    private UsuarioService usuarioService; 
+    private UsuarioService usuarioService;
 
-    @GetMapping("/Crear Cita/{id}")
-    public String crearCita(Cita cita,@PathVariable("id") int id,Model model) {
+    @GetMapping("/Crear Cita/{id_usuarioCita}/{id}/{screen}")
+    public String crearCita(Cita cita,@PathVariable("id_usuarioCita") int id_usuarioCita, @PathVariable("id") int id, @PathVariable("screen") String screen, Model model) {
 
-        cita.setId_usuarios(id);
-        model.addAttribute("cita",cita);
+        Usuario usuario = new Usuario();
+
+        usuario.setId_usuarios(Long.parseLong(String.valueOf(id)));
+        cita.setId_usuarios(id_usuarioCita);
+        
+        model.addAttribute("screen",screen);
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("cita", cita);
         return "actualizar_cita";
 
     }
 
-    @GetMapping("/Guardar Cita/{id}")
-    public String guardar(Cita cita, @PathVariable("id") int id,Model model) {
+    @GetMapping("/Guardar Cita/{id}/{screen}")
+    public String guardar(Cita cita, @PathVariable("id") int id, @PathVariable("screen") String screen, Model model) {
 
         citaService.guardar(cita);
-        return listaCitas(id,model);
+        ControllerUsuario controllerUsuario = new ControllerUsuario();
+
+        if (screen.equals("misClientes")) {
+
+            return "redirect:/Mis Clientes/" + String.valueOf(id); 
+
+        } else {
+
+            return "redirect:/Inicio/" + String.valueOf(id);
+
+        }
 
     }
 
-    @GetMapping("/Las Citas/{id_usuarios}")
-    public String listaCitas(@PathVariable("id_usuarios") int id, Model model) {
+    @GetMapping("/Las Citas/{id_usuarios}/{id_usuario}/{screen}")
+    public String listaCitas(@PathVariable("id_usuarios") int id, @PathVariable("id_usuario") int id_usuario, @PathVariable("screen") String screen, Model model) {
 
         var citas = citaService.getCitasUsuario(id);
 
-        
+        Cita cita = new Cita(); 
+        cita.setId_usuarios(id);
         Usuario usuario = new Usuario();
-        usuario.setId_usuarios(Long.parseLong(String.valueOf(id)));
+        usuario.setId_usuarios(Long.parseLong(String.valueOf(id_usuario)));
 
-        model.addAttribute("usuario",usuario);
+        model.addAttribute("cita",cita);
+        model.addAttribute("screen", screen);
+        model.addAttribute("usuario", usuario);
         model.addAttribute("citas", citas);
 
         return "lista_cita";
@@ -55,12 +74,12 @@ public class ControllerCita {
         return "actualizar_cita";
     }
 
-    @GetMapping("/eliminar/{id_citas}/{id}")
-    public String eliminar(Cita cita, Model model,@PathVariable("id") int id) {
-        
+    @GetMapping("/eliminar/{id_citas}/{id}/{screen}")
+    public String eliminar(Cita cita, Model model, @PathVariable("id") int id, @PathVariable("screen") String screen) {
+
         citaService.eliminar(cita);
-        return listaCitas(id,model);
-        
+        return "redirect:/Inicio/" + String.valueOf(id);
+
     }
 
 }
--- projectoDesarolloWeb/src/main/java/src403/projectFinalPrograWeb/Usuario/ControllerUsuario.java
+++ projectoDesarolloWeb/src/main/java/src403/projectFinalPrograWeb/Usuario/ControllerUsuario.java
@@ -5,11 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @Slf4j
@@ -70,44 +66,53 @@ public class ControllerUsuario {
             model.addAttribute("usuario", usuario);
 
         }
-        
+
         return "Pantalla_Principal";
 
     }
 
     @GetMapping("/Mis Clientes/{id}")
-    public String listaClientes(Model model,@PathVariable("id") int id) {
+    public String listaClientes(Model model, @PathVariable("id") int id) {
 
-        Usuario usuario = new Usuario(); 
+        Usuario usuario = new Usuario();
         usuario.setId_usuarios(Long.parseLong(String.valueOf(id)));
         var usuarios = usuarioService.getUsuarios();
 
-        model.addAttribute("usuario",usuario); 
+        model.addAttribute("usuario", usuario);
         model.addAttribute("usuarios", usuarios);
 
         return "Mis_Clientes";
 
     }
 
-    @GetMapping("/Formulario/{id_usuarios}/{id}")
-    public String formularioCliente(Usuario usuario, Model model,@PathVariable("id") int id) {
+    @GetMapping("/Formulario/{id_usuarios}/{id}/{screen}")
+    public String formularioCliente(Usuario usuario, Model model, @PathVariable("id") int id,@PathVariable("screen") String screen) {
 
         usuario = usuarioService.getUsuario(usuario);
-        Usuario usuarioLog = new Usuario(); 
+        Usuario usuarioLog = new Usuario();
         usuarioLog.setId_usuarios(Long.parseLong(String.valueOf(id)));
         
-        
-        model.addAttribute("usuario",usuarioLog);
+        model.addAttribute("screen",screen);
+        model.addAttribute("usuario", usuarioLog);
         model.addAttribute("usuarioForm", usuario);
         return "Formulario";
 
     }
 
-    @GetMapping("/Guardar Formulario")
-    public String guardarFormulario(Usuario usuario) {
+    @GetMapping("/Guardar Formulario/{id}/{screen}")
+    public String guardarFormulario(Usuario usuario, @PathVariable("id") int id, @PathVariable("screen") String screen, Model model) {
 
         usuarioService.guardar(usuario);
-        return "redirect:/Mis Clientes";
+
+        if (screen.equalsIgnoreCase("misClientes")) {
+
+            return listaClientes(model, id);
+
+        } else {
+
+            return Inicio(Long.parseLong(String.valueOf(id)), model);
+
+        }
 
     }
 
